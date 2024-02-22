5befe7443a44cd6ff387d927987a4a3aac7d3569
Galcoca <galcoca18@gmail.com>
2024-02-21 21:51:22 -0500

    App completed, ready to send to production

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,32 +1,88 @@
+import React, { useState } from "react";
+
+import words from 'lodash.words'
+
 import Result from "./components/ResultComponent";
 import MathOperations from "./components/MathOperationsComponent";
 import Numbers from "./components/NumbersComponent";
 import Functions from "./components/FunctionsComponent";
 
 const App = () => {
-	const onClickButtonFunction = text => (
-		console.log('Button.Clickhandler', text)
-	)
+	const [number, setNumber] = useState('');
 
-	const onClickOperationFunction = operation => (
-		console.log('Operation', operation)
-	)
+	const items = words(number, /[^-^+^*^/]+/g)
 
-	const onClickEqualFunction = equal => (
-		console.log('Equal', equal)
-	)
+	const value = items.length > 0 ? items[items.length-1] : "0";
 
-	const onDeleteFunction = Return => (
-		console.log('Return', Return)
-	)
+	const onClickButtonFunction = addNumber => {
+		if(number === '0'){
+			setNumber(addNumber)
+		} else {
+			setNumber(`${number}${addNumber}`)
+		}
+	}
+
+	const onClickOperationFunction = operation => {
+		setNumber(`${number}${operation}`)
+	}
+
+	const onClickEqualFunction = (equal) => {
+		let calculatedResult = 0;
+		let currentOperation = "+";
+		let currentNumber = 0;
+	  
+		for (let i = 0; i < number.length; i++) {
+		  const char = number[i];
+	  
+		  if (char >= "0" && char <= "9") {
+			currentNumber = currentNumber * 10 + parseInt(char);
+		  }
+	  
+		  if (char === "+" || char === "-" || char === "*" || char === "/") {
+			if (currentOperation === "+") {
+			  calculatedResult += currentNumber;
+			} else if (currentOperation === "-") {
+			  calculatedResult -= currentNumber;
+			} else if (currentOperation === "*") {
+			  calculatedResult *= currentNumber;
+			} else if (currentOperation === "/") {
+			  calculatedResult /= currentNumber;
+			}
+	  
+			currentOperation = char;
+			currentNumber = 0;
+		  }
+		}
+	  
+		if (currentOperation === "+") {
+		  calculatedResult += currentNumber;
+		} else if (currentOperation === "-") {
+		  calculatedResult -= currentNumber;
+		} else if (currentOperation === "*") {
+		  calculatedResult *= currentNumber;
+		} else if (currentOperation === "/") {
+		  calculatedResult /= currentNumber;
+		}
+	  
+		setNumber(calculatedResult.toString());
+	  };
+
+	const onDeleteFunction = Return => {
+		if(number.length > 1){
+			const newNumber = number.substring(0, number.length - 1);
+			setNumber(newNumber);
+		} else {
+			setNumber('0');
+		}
+	}
 
 	const onContentClearFunction = clr => (
-		console.log('Clear', clr)
+		setNumber('0')
 	)
 
 	return (
 		<div className="reactCalculator">
-			<Result value={"0"}/>			
+			<Result value={value}/>			
 			<Numbers onClickNumber={onClickButtonFunction}/>
 			<Functions onDelete={onDeleteFunction} onContentClear={onContentClearFunction}/>
 			<MathOperations onClickOperation={onClickOperationFunction} onClickEqual={onClickEqualFunction}/>
